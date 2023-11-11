6bf34c69002d066af9a0ad8aa1f41068050997a9
HaythemLazaar <haythem.lazaar1@gmail.com>
2023-11-11 08:50:20 +0100

    added assets + sidebar

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,50 @@
+"use client";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { Logout, Setting2 } from "iconsax-react";
+
+export default function Sidebar() {
+  const navItems = [
+    { name: "Dashboard", path: "/" },
+    { name: "My Projects", path: "/projects" },
+    { name: "My Products", path: "/products" },
+    { name: "My configurator", path: "/config" },
+    { name: "3D Library", path: "/library" },
+    { name: "Templates", path: "/templates" },
+  ];
+  const path = usePathname();
+
+  return (
+    <div className="h-[100vh] w-[260px] rounded-e-[10px] bg-surfaceTeal flex flex-col justify-between pl-10 py-10 pb-20">
+      <div className="flex flex-col gap-32">
+        <Image src="Logo.svg" height={30} width={135} alt="The Mashroom Logo" />
+        <ul className="flex flex-col gap-4 w-fit">
+          {navItems.map((item) => {
+            return (
+              <li
+                key={item.name}
+                className={
+                  item.path === "/"
+                    ? "bg-primary text-white my-2 pl-4 py-2 pr-6 w-full rounded cursor-pointer"
+                    : "pl-4 pr-6 w-full rounded cursor-pointer hover:text-primary"
+                }
+              >
+                {item.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="flex flex-col gap-8">
+        <div className="flex gap-4 items-center px-4">
+          <Setting2 size="24" />
+          <h3>Settings</h3>
+        </div>
+        <div className="flex gap-4 items-center px-4">
+          <Logout size="24" className="text-red-500" />
+          <h3 className="text-red-500">Logout</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        surfaceTeal: "#E4EEEF",
+        primary: "#168891",
+        surfaceAmber: "#F4C657",
+        surfacegray: "#F2F2F2",
+        text: "#4C4C4C",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;

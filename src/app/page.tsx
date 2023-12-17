b5db4efdf791fc68be4a0bd25b96b038be8044a4
HaythemLazaar <haythem.lazaar1@gmail.com>
2023-12-17 10:17:17 +0100

    fixed root styling issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Link from "next/link";
 
 export default function Home() {
-  return <><h1>Landing Page</h1><Link href="/home">Proceed To Dashboard</Link></>;
+  return (
+    <main className="px-4">
+      <h1 className="text-3xl font-bold my-5">Landing Page</h1>
+      <Link href="/home" className="text-base">Proceed To Dashboard</Link>
+    </main>
+  );
 }

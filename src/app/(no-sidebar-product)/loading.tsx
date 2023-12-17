31421d21682907719e4376d6c7e162f89c4c5bad
HaythemLazaar <haythem.lazaar1@gmail.com>
2023-12-17 10:23:36 +0100

    added loading component

---
+++ src/app/(no-sidebar-product)/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading(){
+    return (
+        <div className="w-full h-[90vh] flex items-center justify-center">
+            <h1 className="text-xl font-bold">Loading Page...</h1>
+        </div>
+    );
+}
